cc1343b7ad0afc9d309a70529362ff605d3ea98a
Natalia <72099196+Nataliia2@users.noreply.github.com>
2022-07-02 10:18:12 +0300

    задание 2, 3

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const createList = document.querySelector('#ingredients');
+const elements = [];
+
+for (let i = 0; i < ingredients.length; i += 1) { 
+  const ingredient = ingredients[i];
+
+  const listEL = document.createElement('li');
+  listEL.classList.add("item");
+  listEL.textContent = ingredient;
+  elements.push(listEL);
+  console.log(listEL);
+}
+createList.append(...elements);
+
