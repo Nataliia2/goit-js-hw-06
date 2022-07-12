cb5fcfde64c93a29b51c3ca24d766e070bc3b5cc
Natalia <72099196+Nataliia2@users.noreply.github.com>
2022-07-12 08:52:34 +0300

    правки 10 задание

--- js/task-10.js
+++ js/task-10.js
@@ -4,26 +4,23 @@ const btnDestroy = document.querySelector('button[data-destroy]');
 const divBoxEl = document.querySelector('#boxes');
 
 btnCreate.addEventListener('click', onCreateBoxes);
- btnDestroy.addEventListener('click', destroyBoxes);
+btnDestroy.addEventListener('click', destroyBoxes);
 
- function onCreateBoxes() {
-   let numbers = [];
-  for (let i = 1; i <= inputEl.value; i ++) {
-     numbers.push(i);
-   };
-     const createDivs = numbers.map((number) => {
-       const divEl = document.createElement('div');
-       const value = ((30 + divBoxEl.childElementCount*10) + number * 10);
-       divEl.style.width = `${value}px`;
-       divEl.style.height = `${value}px`;
-       divEl.style.backgroundColor = getRandomHexColor();
-       return divEl;
-     }); 
-    
-     divBoxEl.append(...createDivs);
-     console.log(divBoxEl);
+function onCreateBoxes(event) {
+  const amount = inputEl.value;
+  let fragment = new DocumentFragment;
   
- };
+  for (let i = 1; i <= amount; i++) {
+    const createDivEl = document.createElement('div');
+    const value = ((20 + divBoxEl.childElementCount * 10)+ i * 10);
+    createDivEl.style.width = `${value}px`;
+    createDivEl.style.height = `${value}px`;
+    createDivEl.style.backgroundColor = getRandomHexColor();
+
+    fragment.appendChild(createDivEl);
+  };
+    divBoxEl.appendChild(fragment);
+};
 
 function destroyBoxes() {
   inputEl.value = '';
